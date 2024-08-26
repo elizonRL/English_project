ece1a9d194eca8e0e95b740af55e67b8329c8c4d
Elizon Rodrigues <118481749+elizonRL@users.noreply.github.com>
2024-08-26 10:19:45 -0400

    videos componet

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,30 @@
 import { useEffect, useState } from "react";
 import Card from "./components/Card";
 import getDataService from "./services/getData.service";
+import "@justinribeiro/lite-youtube";
+import Video from "./components/Viddeo";
+import YouTube from "./components/icons/Yotube";
 
 const App = () => {
   const [topic, setTopics] = useState([]);
+  const videos = [
+    {
+      id: 1,
+      videoTitle: "Obligation and prohibition",
+      videoId: "Xhd1bEVXoZk",
+    },
+    {
+      id: 2,
+      videoTitle: "Diferencia MUST, HAVE TO, MUSTN'T y DON'T HAVE TO",
+      videoId: "WGQIIBKf0I4",
+    },
+  ];
   useEffect(() => {
     getDataService.getAllData().then((data) => {
-      setTopics(data)
+      setTopics(data);
     });
   }, []);
-  
+
   return (
     <>
       <main>
@@ -19,6 +34,10 @@ const App = () => {
           These are the different topics covered in units 5 and 6 for the exam.
         </p>
         <Card topics={topic} />
+        <section>
+          <h2 className="title-video">List of videos <YouTube/> </h2>
+          <Video videos={videos}/>
+        </section>
       </main>
     </>
   );
